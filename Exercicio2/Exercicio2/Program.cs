cf09ea37616c4ecda2f04dbf1ebb17584294459c
bbraga99 <brunuu.henrique@hotmail.com>
2023-11-05 09:09:39 -0300

    Second exercice add

---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using Exercicio2.Entities;
+using Exercicio2.Services;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data ");
+            Console.Write("Number");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Console.Write("Contract value: ");
+            double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Contract contract = new Contract(number, date, contractValue);
+
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+            ContractService contractService = new ContractService(new PaypalPayment());
+
+            contractService.ProcessContract(contract, months);
+
+            Console.WriteLine("installments:");
+
+            foreach (Installment installment in contract.Installments)
+            {
+                {
+                    Console.WriteLine(installment);
+                }
+            }
+        }
+    }
+}
---
+++ Exercicio2/Exercicio2/Services/ContractService.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.Contracts;
+using Exercicio2.Entities;
+using Contract = Exercicio2.Entities.Contract;
+
+namespace Exercicio2.Services
+{
+    internal class ContractService
+    {
+        private IOnlinePaymentService _onlinePaymentService;
+
+        public ContractService(IOnlinePaymentService onlinePaymentService)
+        {
+            _onlinePaymentService = onlinePaymentService;
+        }
+        public void ProcessContract(Contract contract, int months)
+        {
+            double basicQuota = contract.TotalValue / months;
+
+            for(int i = 1; i <= months; i++) 
+            {
+                DateTime date = contract.Date.AddMonths(i);
+                double updatedQuota = basicQuota + _onlinePaymentService.Interest(basicQuota,i);
+                double fullQUota = updatedQuota + _onlinePaymentService.PaymentFee(updatedQuota);
+                contract.AddInstallment(new Installment(date, fullQUota));  
+            }
+
+        }
+    }
+}
